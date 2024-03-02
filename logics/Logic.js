3f37ec295a0d7ba06ff641406cf46dcace804285
trishaDas13 <trisha.das1308@gmail.com>
2024-03-02 22:09:56 +0530

    backend logic done

---
+++ logics/Logic.js
@@ -0,0 +1,15 @@
+// import express from  "express";
+// import {nanoid} from "nanoid";
+
+// const router = express.Router();
+
+// router.post( "/URLshortner", (req, res) =>{
+//     console.log(req.body);
+//     // const urlCode = nanoid();
+//     res.json({
+//         success:true,
+//         message: 'url shortner'
+//     })
+// })
+
+// module.exports = router;
---
+++ script.js
@@ -0,0 +1,52 @@
+// const express = require("express");
+import express from "express";
+import { nanoid } from "nanoid";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+app.use(express.json());
+
+const validURL = (url) =>{
+    try{
+        new URL(url);
+        return true;
+    }catch(error){
+        return false;
+    }
+}
+
+//todo: post API calling for generate and store short URL
+app.post("/URLshortner", (req, res) => {
+  const longURL = req.body.url;
+  const urlCode = nanoid(6);
+  const valid = validURL(longURL)
+
+  //* store the urlcode and long url in a file
+  const fileRes = fs.readFileSync("URLdata.json");
+  const fileData = JSON.parse(fileRes.toString());
+  fileData[urlCode] = longURL;
+  fs.writeFileSync("URLdata.json", JSON.stringify(fileData));
+
+  res.json({
+    success: true,
+    message: `http://localhost:5000/${urlCode}`,
+  });
+});
+
+//todo: get API for  redirect to original URL
+app.get("/:url_id", (req, res) => {
+  const fileRes = fs.readFileSync("URLdata.json");
+  const fileData = JSON.parse(fileRes.toString());
+
+  const longURL = fileData[req.params.url_id];
+  res.redirect(longURL);
+
+  res.json({
+    suc: "true",
+  });
+});
+
+app.listen(5000, (req, res) => {
+  console.log("server is running at port no. 5000");
+});
