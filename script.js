f3867d31f65391c61909dd4a151252702cd4cf02
trishaDas13 <trisha.das1308@gmail.com>
2024-03-04 18:15:16 +0530

    error handled

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 import express from "express";
 import { nanoid } from "nanoid";
 import fs from "fs";
+import { fileURLToPath } from "url";
 import path from "path";
+import bodyParser from "body-parser";
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filepath);
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 const validURL = (url) => {
@@ -16,25 +22,30 @@ const validURL = (url) => {
   }
 };
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 //todo: post API calling for generate and store short URL
 app.post("/URLshortner", (req, res) => {
-  const longURL = req.body.url;
-  const customURL = req.body.name;
+  const longLink = req.body.longURL;
+  const customURL = req.body.customURL;
   const urlCode = customURL ? customURL : nanoid(6);
-  const valid = validURL(longURL);
+  const valid = validURL(longLink);
 
-  //* store the urlcode and long url in a file
-  const fileRes = fs.readFileSync("URLdata.json");
-  const fileData = JSON.parse(fileRes.toString());
-  fileData[urlCode] = longURL;
-  fs.writeFileSync("URLdata.json", JSON.stringify(fileData));
-  // if (valid)
-  //   return res.status(400).send({ success: false, Error: "Invalid URL!" });
-
-  res.json({
-    success: true,
-    message: `http://localhost:5000/${urlCode}`,
-  });
+  if (valid) {
+    //* store the urlcode and long url in a file
+    const fileRes = fs.readFileSync("URLdata.json");
+    const fileData = JSON.parse(fileRes.toString());
+    fileData[urlCode] = longLink;
+    fs.writeFileSync("URLdata.json", JSON.stringify(fileData));
+    res.json({
+      success: true,
+      message: `http://localhost:5000/${urlCode}`,
+    });
+  } else {
+    return res.status(400).send({ success: false, Error: "Invalid URL!" });
+  }
 });
 
 //todo: get API for  redirect to original URL
@@ -43,11 +54,11 @@ app.get("/:url_id", (req, res) => {
   const fileData = JSON.parse(fileRes.toString());
 
   const longURL = fileData[req.params.url_id];
-  res.redirect(longURL);
-
-  res.json({
-    suc: "true",
-  });
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).json({ error: "URL not found" });
+  }
 });
 
 app.listen(5000, (req, res) => {
